3b9bf45c20016a02eb96665fb3a875be68b40c6b
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-14 20:14:40 -0300

    Add replacemente through regex

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::{env, fs};
 
 use text_colorizer::*;
 
+use regex::Regex;
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
+
 fn main() {
     let args = parse_args();
 
@@ -18,7 +24,15 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(
